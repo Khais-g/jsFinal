9db54aaaac25634f2f8b1fdc721f50152dbc233d
Khais-g <KHAIS.GAHROURI@latoile.em-lyon.com>
2025-05-21 16:48:52 +0200

    salut

--- script.js
+++ script.js
@@ -22,12 +22,18 @@ fetch("https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.g
         bouton.textContent = data.texteAppelAction;
         div.appendChild(bouton);
 
+
+        data["avantagesClients"].forEach(Element=> {
         const arguments = document.createElement("p");
-        arguments.textContent = data.avantagesClients;
+        arguments.textContent = Element;
         div.appendChild(arguments);
+        })
+  
     }
      //pour afficher le nom de l'entreprise, 
     //sa phrase d'accroche et un bouton d'appel à l'action.
+
+    
 
     entreprise();
 
@@ -35,31 +41,60 @@ fetch("https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.g
     const sec = document.createElement("section");
 
     data["produits"].forEach(produits => {
-         const card = document.createElement("div");
+        const card = document.createElement("div");
         card.classList.add("card");
+        document.body.appendChild(sec);
+        sec.appendChild(card);
 
-        const name = document.createElement("p");
-            name.textContent =  produits.nom;
+        const name = document.createElement("h2");
+            name.textContent = produits.nom;
             card.appendChild(name);
 
-        const desk = document.createElement("h2");
+        const desk = document.createElement("p");
             desk.textContent = produits.description;
             card.appendChild(desk);
 
-        
+        const img = document.createElement("img");
+            img.src = produits["image-url"];
+            card.appendChild(img);
+          
+
     });
     }
 
-    products();   
+    products();  
+
+
+
+
+    function temoin (){
+    const tion = document.createElement("section");
+
+    data["temoignages"].forEach(deposition => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        document.body.appendChild(tion);
+        tion.appendChild(card);
+
+        const prenom = document.createElement("h3");
+            prenom.textContent = deposition.prenom;
+            card.appendChild(prenom);
+
+        const type = document.createElement("h4");
+            type.textContent = deposition.typesExperience;
+            card.appendChild(type);
+
+        const com = document.createElement("p");
+           com.textContent = deposition.commentaire;
+            card.appendChild(com);
+          
+
+    });
+    }
+
+    temoin()
 
         
-        
-       
-
-   
-
-
-
 
 
 })
